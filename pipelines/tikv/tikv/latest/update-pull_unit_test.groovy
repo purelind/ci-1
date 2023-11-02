5737d8182584150e332d6e39d34ec8294fe49f6c
purelind <purelind@gmail.com>
2023-11-02 15:27:54 +0800

    chore: comment some code to test build test binary

--- pipelines/tikv/tikv/latest/update-pull_unit_test.groovy
+++ pipelines/tikv/tikv/latest/update-pull_unit_test.groovy
@@ -22,11 +22,8 @@ spec:
       tty: true
       resources:
         requests:
-          memory: 8Gi
-          cpu: "6"
-        limits:
-          memory: 8Gi
-          cpu: "6"
+          memory: 20Gi
+          cpu: "8"
       volumeMounts:
       - mountPath: "/home/jenkins/agent/memvolume"
         name: "volume-memory"
@@ -143,99 +140,99 @@ pipeline {
                                     # Cargo metadata
                                     cargo metadata --format-version 1 > test-metadata.json
                                 """
-                                sh label: 'Post build artifact', script: """
-                                    tar czf test-artifacts.tar.gz test-binaries test-binaries.json test-metadata.json Cargo.toml cmd src tests components .config `ls target/*/deps/*plugin.so 2>/dev/null`
-                                    ls -alh test-artifacts.tar.gz
-                                """
-                                stash name: 'test-artifacts', includes: 'test-artifacts.tar.gz'
+                                // sh label: 'Post build artifact', script: """
+                                //     tar czf test-artifacts.tar.gz test-binaries test-binaries.json test-metadata.json Cargo.toml cmd src tests components .config `ls target/*/deps/*plugin.so 2>/dev/null`
+                                //     ls -alh test-artifacts.tar.gz
+                                // """
+                                // stash name: 'test-artifacts', includes: 'test-artifacts.tar.gz'
                             }
                         }
                     }
                 }
-                stage('lint') {
-                    steps {
-                        dir("tikv") {
-                            retry(2) {
-                                sh label: 'Run lint: format', script: """
-                                    cd \$HOME/tikv-src
-                                    export RUSTFLAGS=-Dwarnings
-                                    make format
-                                    git diff --quiet || (git diff; echo Please make format and run tests before creating a PR; exit 1)
-                                """
-                                sh label: 'Run lint: clippy', script: """
-                                    cd \$HOME/tikv-src
-                                    export RUSTFLAGS=-Dwarnings
-                                    export FAIL_POINT=1
-                                    export ROCKSDB_SYS_SSE=1
-                                    export RUST_BACKTRACE=1
-                                    export LOG_LEVEL=INFO
-                                    echo using gcc 8
-                                    source /opt/rh/devtoolset-8/enable
-                                    make clippy || (echo Please fix the clippy error; exit 1)
-                                """
-                            }
-                        }
-                    }
-                }
+                // stage('lint') {
+                //     steps {
+                //         dir("tikv") {
+                //             retry(2) {
+                //                 sh label: 'Run lint: format', script: """
+                //                     cd \$HOME/tikv-src
+                //                     export RUSTFLAGS=-Dwarnings
+                //                     make format
+                //                     git diff --quiet || (git diff; echo Please make format and run tests before creating a PR; exit 1)
+                //                 """
+                //                 sh label: 'Run lint: clippy', script: """
+                //                     cd \$HOME/tikv-src
+                //                     export RUSTFLAGS=-Dwarnings
+                //                     export FAIL_POINT=1
+                //                     export ROCKSDB_SYS_SSE=1
+                //                     export RUST_BACKTRACE=1
+                //                     export LOG_LEVEL=INFO
+                //                     echo using gcc 8
+                //                     source /opt/rh/devtoolset-8/enable
+                //                     make clippy || (echo Please fix the clippy error; exit 1)
+                //                 """
+                //             }
+                //         }
+                //     }
+                // }
             }
         }
-        stage('Tests') {
-            matrix {
-                axes {
-                    axis {
-                        name 'CHUNK_SUFFIC'
-                        values 1, 2
-                    }
-                }
-                agent{
-                    kubernetes {
-                        namespace K8S_NAMESPACE
-                        // yamlFile POD_TEMPLATE_FILE
-                        yaml podYAML
-                        defaultContainer 'runner'
-                        workspaceVolume emptyDirWorkspaceVolume(memory: true)
-                    }
-                }
-                stages {
-                    stage("Test") {
-                        options { timeout(time: 15, unit: 'MINUTES') }
-                        steps {
-                            dir('tikv') {
-                                unstash 'test-artifacts'
-                                sh """
-                                cd \$HOME/tikv-src
-                                cp -r $WORKSPACE/tikv/* ./
-                                ls -alh
-                                export RUSTFLAGS=-Dwarnings
-                                export FAIL_POINT=1
-                                export RUST_BACKTRACE=1
-                                export MALLOC_CONF=prof:true,prof_active:false
-                                export CI=1  # TODO: remove this
-                                export LOG_FILE=$WORKSPACE/tikv/target/my_test.log
+        // stage('Tests') {
+        //     matrix {
+        //         axes {
+        //             axis {
+        //                 name 'CHUNK_SUFFIC'
+        //                 values 1, 2
+        //             }
+        //         }
+        //         agent{
+        //             kubernetes {
+        //                 namespace K8S_NAMESPACE
+        //                 // yamlFile POD_TEMPLATE_FILE
+        //                 yaml podYAML
+        //                 defaultContainer 'runner'
+        //                 workspaceVolume emptyDirWorkspaceVolume(memory: true)
+        //             }
+        //         }
+        //         stages {
+        //             stage("Test") {
+        //                 options { timeout(time: 15, unit: 'MINUTES') }
+        //                 steps {
+        //                     dir('tikv') {
+        //                         unstash 'test-artifacts'
+        //                         sh """
+        //                         cd \$HOME/tikv-src
+        //                         cp -r $WORKSPACE/tikv/* ./
+        //                         ls -alh
+        //                         export RUSTFLAGS=-Dwarnings
+        //                         export FAIL_POINT=1
+        //                         export RUST_BACKTRACE=1
+        //                         export MALLOC_CONF=prof:true,prof_active:false
+        //                         export CI=1  # TODO: remove this
+        //                         export LOG_FILE=$WORKSPACE/tikv/target/my_test.log
 
-                                if cargo nextest run -P ci --binaries-metadata test-binaries.json --cargo-metadata test-metadata.json --partition count:${chunk_suffix}/${CHUNK_COUNT} ${EXTRA_NEXTEST_ARGS}; then
-                                    echo "test pass"
-                                else
-                                    # test failed
-                                    gdb -c core.* -batch -ex "info threads" -ex "thread apply all bt"
-                                    exit 1
-                                fi
-                                """
-                            }
-                        }
-                        post {
-                            failure {
-                                sh label: "collect logs", script: """
-                                    ls /home/jenkins/tikv-src/target/
-                                    tar -cvzf log-${CHUNK_SUFFIC}.tar.gz \$(find /home/jenkins/tikv-src/target/ -type f -name "*.log")    
-                                    ls -alh  log-${CHUNK_SUFFIC}.tar.gz  
-                                """
-                                archiveArtifacts artifacts: "log-${CHUNK_SUFFIC}.tar.gz", fingerprint: true 
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        //                         if cargo nextest run -P ci --binaries-metadata test-binaries.json --cargo-metadata test-metadata.json --partition count:${chunk_suffix}/${CHUNK_COUNT} ${EXTRA_NEXTEST_ARGS}; then
+        //                             echo "test pass"
+        //                         else
+        //                             # test failed
+        //                             gdb -c core.* -batch -ex "info threads" -ex "thread apply all bt"
+        //                             exit 1
+        //                         fi
+        //                         """
+        //                     }
+        //                 }
+        //                 post {
+        //                     failure {
+        //                         sh label: "collect logs", script: """
+        //                             ls /home/jenkins/tikv-src/target/
+        //                             tar -cvzf log-${CHUNK_SUFFIC}.tar.gz \$(find /home/jenkins/tikv-src/target/ -type f -name "*.log")    
+        //                             ls -alh  log-${CHUNK_SUFFIC}.tar.gz  
+        //                         """
+        //                         archiveArtifacts artifacts: "log-${CHUNK_SUFFIC}.tar.gz", fingerprint: true 
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
     }
 }
